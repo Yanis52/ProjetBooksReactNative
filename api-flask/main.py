a2af766a09a14f10c1b9b8610797957017c88912
Yanis Habarek <22010593@etu.unicaen.fr>
2024-09-05 14:59:29 +0200

    api

--- api-flask/main.py
+++ api-flask/main.py
@@ -1,144 +1,125 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import psycopg2
 
 app = Flask(__name__)
 CORS(app)
 
-# Exemple de base de données en mémoire (inventaire de produits)
-products = [
-    {
-        'id': 1,
-        'name': 'Produit A',
-        'description': 'Description du produit A',
-        'quantity': 100,
-        'price': 29.99,
-        'image_url': 'https://example.com/productA.jpg'
-    },
-    {
-        'id': 2,
-        'name': 'Produit B',
-        'description': 'Description du produit B',
-        'quantity': 50,
-        'price': 59.99,
-        'image_url': 'https://example.com/productB.jpg'
-    },
-    {
-        'id': 3,
-        'name': 'Produit C',
-        'description': 'Description du produit C',
-        'quantity': 200,
-        'price': 19.99,
-        'image_url': 'https://example.com/productC.jpg'
-    },
-    {
-        'id': 4,
-        'name': 'Produit D',
-        'description': 'Description du produit D',
-        'quantity': 75,
-        'price': 89.99,
-        'image_url': 'https://example.com/productD.jpg'
-    },
-    {
-        'id': 5,
-        'name': 'Produit E',
-        'description': 'Description du produit E',
-        'quantity': 20,
-        'price': 120.00,
-        'image_url': 'https://example.com/productE.jpg'
-    },
-    {
-        'id': 6,
-        'name': 'Produit F',
-        'description': 'Description du produit F',
-        'quantity': 150,
-        'price': 45.00,
-        'image_url': 'https://example.com/productF.jpg'
-    },
-    {
-        'id': 7,
-        'name': 'Produit G',
-        'description': 'Description du produit G',
-        'quantity': 300,
-        'price': 10.00,
-        'image_url': 'https://example.com/productG.jpg'
-    },
-    {
-        'id': 8,
-        'name': 'Produit H',
-        'description': 'Description du produit H',
-        'quantity': 90,
-        'price': 99.99,
-        'image_url': 'https://example.com/productH.jpg'
-    },
-    {
-        'id': 9,
-        'name': 'Produit I',
-        'description': 'Description du produit I',
-        'quantity': 45,
-        'price': 35.00,
-        'image_url': 'https://example.com/productI.jpg'
-    },
-    {
-        'id': 10,
-        'name': 'Produit J',
-        'description': 'Description du produit J',
-        'quantity': 60,
-        'price': 55.50,
-        'image_url': 'https://example.com/productJ.jpg'
-    }
-]
+# Connexion à la base de données PostgreSQL sur AlwaysData
+def get_db_connection():
+    conn = psycopg2.connect(
+        host='postgresql-yanis52.alwaysdata.net',
+        database='yanis52_db',
+        user='yanis52',
+        password='J@B$qwvH37zrWbY'
+    )
+    return conn
 
-
-# Récupérer tous les produits
+# je recup tous les produits
 @app.route('/products', methods=['GET'])
 def get_products():
-    return jsonify(products)
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM products;')
+    products = cur.fetchall()
+    cur.close()
+    conn.close()
 
-# Récupérer un produit par ID
+    # Je les produits en dictionnaires pour les retourner en JSON
+    products_list = [
+        {'id': p[0], 'name': p[1], 'description': p[2], 'quantity': p[3], 'price': p[4], 'image_url': p[5]} 
+        for p in products
+    ]
+
+    return jsonify(products_list)
+
+# Recup un produit par ID
 @app.route('/products/<int:product_id>', methods=['GET'])
 def get_product(product_id):
-    product = next((product for product in products if product['id'] == product_id), None)
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM products WHERE id = %s;', (product_id,))
+    product = cur.fetchone()
+    cur.close()
+    conn.close()
+
     if product is None:
         return jsonify({'message': 'Produit non trouvé'}), 404
-    return jsonify(product)
 
-# Ajouter un nouveau produit
+    product_dict = {
+        'id': product[0], 
+        'name': product[1], 
+        'description': product[2], 
+        'quantity': product[3], 
+        'price': product[4], 
+        'image_url': product[5]
+    }
+
+    return jsonify(product_dict)
+
+# Add un nouveau produit
 @app.route('/products', methods=['POST'])
 def add_product():
     new_product = request.json
-    new_product['id'] = len(products) + 1
-    products.append(new_product)
+
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('INSERT INTO products (name, description, quantity, price, image_url) VALUES (%s, %s, %s, %s, %s) RETURNING id;',
+                (new_product['name'], new_product['description'], new_product['quantity'], new_product['price'], new_product['image_url']))
+    new_id = cur.fetchone()[0]
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    new_product['id'] = new_id
     return jsonify(new_product), 201
 
 # Mettre à jour un produit
 @app.route('/products/<int:product_id>', methods=['PUT'])
 def update_product(product_id):
-    product = next((product for product in products if product['id'] == product_id), None)
-    if product is None:
-        return jsonify({'message': 'Produit non trouvé'}), 404
-    
     updated_data = request.json
-    product.update(updated_data)
-    return jsonify(product)
+
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('UPDATE products SET name = %s, description = %s, quantity = %s, price = %s, image_url = %s WHERE id = %s;',
+                (updated_data['name'], updated_data['description'], updated_data['quantity'], updated_data['price'], updated_data['image_url'], product_id))
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return jsonify({'message': 'Produit mis à jour'})
 
 # Supprimer un produit
 @app.route('/products/<int:product_id>', methods=['DELETE'])
 def delete_product(product_id):
-    product = next((product for product in products if product['id'] == product_id), None)
-    if product is None:
-        return jsonify({'message': 'Produit non trouvé'}), 404
-    
-    products.remove(product)
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('DELETE FROM products WHERE id = %s;', (product_id,))
+    conn.commit()
+    cur.close()
+    conn.close()
+
     return jsonify({'message': 'Produit supprimé'})
 
-# Rechercher des produits par nom pour la searchbar
+# Rechercher des produits par nom
 @app.route('/products/search', methods=['GET'])
 def search_products():
     query = request.args.get('q', '')
 
-    results = [product for product in products if query.lower() in product['name'].lower()]
-    
-    return jsonify(results)
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM products WHERE LOWER(name) LIKE LOWER(%s);", ('%' + query + '%',))
+    products = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    # Transformer les résultats en dictionnaires pour JSON
+    products_list = [
+        {'id': p[0], 'name': p[1], 'description': p[2], 'quantity': p[3], 'price': p[4], 'image_url': p[5]} 
+        for p in products
+    ]
+
+    return jsonify(products_list)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
